a84db0f46dc3ac10953b4798ba35053817108f60
ted.coderman <ted.coderman@agilethought.com>
2019-05-24 12:36:09 -0400

    demo working

--- WordWebAddIn1Web/Home.js
+++ WordWebAddIn1Web/Home.js
@@ -7,115 +7,58 @@
     // The initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
         $(document).ready(function () {
-            // Initialize the FabricUI notification mechanism and hide it
-            var element = document.querySelector('.ms-MessageBanner');
-            messageBanner = new fabric.MessageBanner(element);
-            messageBanner.hideBanner();
-
-            // If not using Word 2016, use fallback logic.
-            if (!Office.context.requirements.isSetSupported('WordApi', '1.1')) {
-                $("#template-description").text("This sample displays the selected text.");
-                $('#button-text').text("Display!");
-                $('#button-desc').text("Display the selected text");
-                
-                $('#highlight-button').click(displaySelectedText);
-                return;
-            }
-
-            $("#template-description").text("This sample highlights the longest word in the text you have selected in the document.");
-            $('#button-text').text("Highlight!");
-            $('#button-desc').text("Highlights the longest word.");
-            
-            loadSampleData();
-
-            // Add a click event handler for the highlight button.
-            $('#highlight-button').click(hightlightLongestWord);
+            Word.run(function (context) {
+                context.document.body.clear();
+                var cc = context.document.body
+                    .insertParagraph(
+                        "Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.",
+                        "Start"
+                    )
+                    .insertContentControl();
+                context.sync().then(function() {
+                    setInterval(readDocument, 5000);
+                });
+            });
         });
     };
 
-    function loadSampleData() {
-        // Run a batch operation against the Word object model.
+    function readDocument() {
         Word.run(function (context) {
-            // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a commmand to clear the contents of the body.
-            body.clear();
-            // Queue a command to insert text into the end of the Word document body.
-            body.insertText(
-                "This is a sample text inserted in the document",
-                Word.InsertLocation.end);
-
-            // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
-            return context.sync();
-        })
-        .catch(errorHandler);
-    }
-
-    function hightlightLongestWord() {
-        Word.run(function (context) {
-            // Queue a command to get the current selection and then
-            // create a proxy range object with the results.
-            var range = context.document.getSelection();
-            
-            // This variable will keep the search results for the longest word.
-            var searchResults;
-            
-            // Queue a command to load the range selection result.
-            context.load(range, 'text');
-
-            // Synchronize the document state by executing the queued commands
-            // and return a promise to indicate task completion.
-            return context.sync()
-                .then(function () {
-                    // Get the longest word from the selection.
-                    var words = range.text.split(/\s+/);
-                    var longestWord = words.reduce(function (word1, word2) { return word1.length > word2.length ? word1 : word2; });
-
-                    // Queue a search command.
-                    searchResults = range.search(longestWord, { matchCase: true, matchWholeWord: true });
-
-                    // Queue a commmand to load the font property of the results.
-                    context.load(searchResults, 'font');
-                })
-                .then(context.sync)
-                .then(function () {
-                    // Queue a command to highlight the search results.
-                    searchResults.items[0].font.highlightColor = '#FFFF00'; // Yellow
-                    searchResults.items[0].font.bold = true;
-                })
-                .then(context.sync);
-        })
-        .catch(errorHandler);
-    } 
-
-
-    function displaySelectedText() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
+            var contentControls = context.document.body.contentControls;
+            context.load(contentControls, 'title');
+            return context.sync().then(function () {
+                clearContent();
+                if (contentControls.items.length === 0) {
+                    writeContent("No Content Controls found.");
+                    return;
                 }
+                var ooxmlContent = [];
+                for (var i = 0; i < contentControls.items.length; ++i) {
+                    writeContent(contentControls.items[i].title);
+                    // reading the ooxml causes the UI to iterate over every content control
+                    ooxmlContent.push(contentControls.items[i].getOoxml());
+                }
+                context.sync().then(function () {
+                    for (var j = 0; j < ooxmlContent.length; ++j) {
+                        var contentText = ooxmlContent[j].value;
+                        console.log(contentText);
+                    }
+                });
             });
+        })
+        .catch (function (error) {
+            console.log('Error: ' + JSON.stringify(error));
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            }
+        });
     }
 
-    //$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
-    function errorHandler(error) {
-        // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
-        showNotification("Error:", error);
-        console.log("Error: " + error);
-        if (error instanceof OfficeExtension.Error) {
-            console.log("Debug info: " + JSON.stringify(error.debugInfo));
-        }
+    function writeContent(text) {
+        document.getElementById("message").innerHTML += "<br>" + text;
     }
 
-    // Helper function for displaying notifications
-    function showNotification(header, content) {
-        $("#notification-header").text(header);
-        $("#notification-body").text(content);
-        messageBanner.showBanner();
-        messageBanner.toggleExpansion();
+    function clearContent() {
+        document.getElementById("message").innerHTML = "";
     }
 })();
